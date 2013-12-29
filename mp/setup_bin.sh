ad37897fce7d4184661fbb806d4c1eba3f9da945
Airuike <airuike@dnbrown.net>
2013-12-28 23:53:38 -0500

    updated makefile/env for prep

--- mp/setup_bin.sh
+++ mp/setup_bin.sh
@@ -1,15 +1,23 @@
 #!/bin/sh
 
-if [ -z "$BINDIR" ]
+if [ -z "$BINDIR" ] || [ -z "$GAMEDST" ]
 then
-	echo "set BINDIR before running"
+	echo "set BINDIR, GAMEDST before running"
 	exit 1
 fi
 
-GAMEDIR=game/game_src
+rm -rf $GAMEDST/maps
+rm -rf $GAMEDST/models
+rm -rf $GAMEDST/sound
+rm -rf $GAMEDST/particles
+rm -rf $GAMEDST/materials
+rm -rf $GAMEDST/resources
+rm -rf $GAMEDST/shaders
 
-ln -s $BINDIR/mapsrc $GAMEDIR/mapsrc
-ln -s $BINDIR/modelsrc $GAMEDIR/modelsrc
-ln -s $BINDIR/sound $GAMEDIR/sound
-ln -s $BINDIR/particles $GAMEDIR/particles
-ln -s $BINDIR/materialsrc $GAMEDIR/materialsrc
+ln -s $BINDIR/maps $GAMEDST/maps
+ln -s $BINDIR/models $GAMEDST/models
+ln -s $BINDIR/sound $GAMEDST/sound
+ln -s $BINDIR/particles $GAMEDST/particles
+ln -s $BINDIR/materials $GAMEDST/materials
+ln -s $BINDIR/resources $GAMEDST/resources
+ln -s $BINDIR/shaders $GAMEDST/shaders
---
+++ mp/update_dist.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+DIST=$1
+
+if [ -z "$BINDIR" ] || [ -z "$GAMEDST" ]
+then
+	echo "set BINDIR, GAMEDST before running"
+	exit 1
+fi
+
+if [ -z "$1" ]
+then
+	echo "specify a directory to place dist files"
+	exit 1
+fi
+
+check_and_link() {
+	if [ ! -e "$DIST/$1" ]
+	then
+		ln -s $BINDIR/$1 $DIST/$1 || exit 1
+	fi
+}
+
+mkdir -p $DIST/bin
+
+check_and_link maps
+check_and_link models
+check_and_link sound
+check_and_link particles
+check_and_link materials
+check_and_link resources
+check_and_link shaders
+
+rsync $BINDIR/gameinfo.txt $DIST/gameinfo.txt
+rsync $BINDIR/darkness.fgd $DIST/darkness.fgd
