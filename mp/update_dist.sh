afa4b11b6bae05fa614169a2301d1de8ad813807
Airuike <airuike@dnbrown.net>
2014-07-12 16:24:39 -0400

    update_dist.sh supports vpk

--- mp/update_dist.sh
+++ mp/update_dist.sh
@@ -24,14 +24,20 @@ check_and_link() {
 mkdir -p $DIST/bin
 
 check_and_link maps
-check_and_link models
+#check_and_link models
 check_and_link sound
-check_and_link particles
-check_and_link materials
+#check_and_link particles
+#check_and_link materials
 check_and_link resource
 check_and_link shaders
 check_and_link scripts
 check_and_link cfg
 
-rsync $BINDIR/gameinfo.txt $DIST/gameinfo.txt
+rsync $BINDIR/gameinfo_vpk.txt $DIST/gameinfo.txt
 rsync $BINDIR/darkness.fgd $DIST/darkness.fgd
+rsync $BINDIR/materials_dir.vpk $DIST/materials_dir.vpk
+rsync $BINDIR/materials_000.vpk $DIST/materials_000.vpk
+rsync $BINDIR/particles_dir.vpk $DIST/particles_dir.vpk
+rsync $BINDIR/particles_000.vpk $DIST/particles_000.vpk
+rsync $BINDIR/models_dir.vpk $DIST/models_dir.vpk
+rsync $BINDIR/models_000.vpk $DIST/models_000.vpk
